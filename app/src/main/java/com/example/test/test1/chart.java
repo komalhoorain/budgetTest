b67beb3230a59672be42a486e6bbdc6ad9c36064
komalhoorain <komal.uet@gmail.com>
2016-09-02 05:05:14 -0700

    change chart library

--- app/src/main/java/com/example/test/test1/chart.java
+++ app/src/main/java/com/example/test/test1/chart.java
@@ -3,19 +3,13 @@ package com.example.test.test1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.view.Chart;
-import lecho.lib.hellocharts.view.PieChartView;
 
 public class chart extends AppCompatActivity {
-    PieChartView piechart;
-    PieChartData piedata;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
-        //
-        piechart = (PieChartView) findViewById(R.id.chart);
 
     }
 }
